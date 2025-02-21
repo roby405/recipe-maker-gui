bfb97a701850718c9505be90c9b2fc805b6bf081
roby405 <robep2341@gmail.com>
2025-02-21 03:34:54 +0200

    added more features to the GUI and started using states

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use eframe::egui;
+use egui::FontFamily::Proportional;
+use egui::FontId;
+use egui::TextStyle::*;
 use std::collections::HashMap;
 
 #[derive(Clone, Default)]
@@ -10,7 +13,7 @@ struct Recipe {
     instructions: Vec<String>,
 }
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
 enum AppState {
     MainMenu,
     AddRecipe(i32),
@@ -20,12 +23,16 @@ enum AppState {
     DeleteRecipe(Recipe),
 }
 
+impl Default for AppState {
+    fn default() -> Self { AppState::MainMenu }
+}
+
 fn main() -> Result<(), eframe::Error> {
     // Create a new native app window
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-        .with_inner_size([400.0, 300.0])
-        .with_min_inner_size([300.0, 220.0]),// Set window size
+        .with_inner_size([960.0, 630.0])
+        .with_min_inner_size([640.0, 425.0]),// Set window size
         ..Default::default()
     };
 
@@ -41,14 +48,15 @@ fn main() -> Result<(), eframe::Error> {
 #[derive(Default)]
 struct MyApp {
     menu: AppState,
-    name: String,       // A text input field
-    age: u32,           // A number input field
-    is_happy: bool,     // A checkbox
+    recipes: Vec<Recipe>
 }
 
 impl MyApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default()
+        Self {
+            menu: AppState::MainMenu,
+            recipes: Vec::new(),
+        }
     }
 }
 
@@ -58,31 +66,70 @@ impl eframe::App for MyApp {
         let a:i32 = 5;
         // Create a new window
         egui::CentralPanel::default().show(ctx, |ui| {
+            let mut style = (*ctx.style()).clone();
+            style.text_styles = [
+                (Heading, FontId::new(30.0, Proportional)),
+                (Name("Heading2".into()), FontId::new(25.0, Proportional)),
+                (Name("Context".into()), FontId::new(23.0, Proportional)),
+                (Body, FontId::new(18.0, Proportional)),
+                (Monospace, FontId::new(14.0, Proportional)),
+                (Button, FontId::new(24.0, Proportional)),
+                (Small, FontId::new(10.0, Proportional)),
+              ].into();
+            ui.style_mut().text_styles = style.text_styles;
+
             // Add a heading
-            ui.heading("My First egui App");
+            egui::SidePanel::left("option_menu")
+                .resizable(true)
+                .default_width(300.0)
+                .show_inside(ui, |ui| {
 
+                ui.vertical_centered(|ui| {
+                    ui.heading("Recipe Maker");
+                    ui.separator();
+                    if ui.button("Add recipe").clicked() {
+                        self.menu = AppState::AddRecipe(0);
+                    }
+                    if ui.button("Edit recipe").clicked() {
+                        self.menu = AppState::EditRecipe(Recipe::default());
+                    }
+                    if ui.button("Delete recipe").clicked() {
+                        self.menu = AppState::DeleteRecipe(Recipe::default());
+                    }
+                    ui.separator();
+                    if ui.button("View recipe").clicked() {
+                        self.menu = AppState::ViewDetails(Recipe::default());
+                    }
+                    if ui.button("View all recipes").clicked() {
+                        self.menu = AppState::ViewRecipes(Vec::new());
+                    }
+                });
+            });
+            ui.vertical_centered(|ui| {
+                ui.heading("TEXT AREA");
+            });
             // Add a text input field for the name
-            ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
-            });
+            // ui.horizontal(|ui| {
+            //     ui.label("Your name: ");
+            //     // ui.text_edit_singleline(&mut self.name);
+            // });
 
-            // Add a number input field for the age
-            ui.horizontal(|ui| {
-                ui.label("Your age: ");
-                ui.add(egui::DragValue::new(&mut self.age));
-            });
+            // // Add a number input field for the age
+            // ui.horizontal(|ui| {
+            //     ui.label("Your age: ");
+            //     // ui.add(egui::DragValue::new(&mut self.age));
+            // });
 
             // Add a checkbox for happiness
-            ui.checkbox(&mut self.is_happy, "Are you {a} happy?");
+            // ui.checkbox(&mut self.is_happy, "Are you {a} happy?");
 
             // Add a button to display the entered data
-            if ui.button("Submit").clicked() {
-                println!(
-                    "Name: {}, Age: {}, Happy: {}",
-                    self.name, self.age, self.is_happy
-                );
-            }
+            // if ui.button("Submit").clicked() {
+            //     println!(
+            //         "Name: {}, Age: {}, Happy: {}",
+            //         self.name, self.age, self.is_happy
+            //     );
+            // }
         });
     }
 }
